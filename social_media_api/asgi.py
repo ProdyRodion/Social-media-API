228be084317daf4a26516d71d0aa5f28bde9b1ee
Rodion Prodaivoda <hapyto371@gmail.com>
2023-04-21 14:36:00 +0300

    tuned settings

--- social_media_api/asgi.py
+++ social_media_api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Social_media_API project.
+ASGI config for social_media_api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Social_media_API.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_media_api.settings')
 
 application = get_asgi_application()
--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Social_media_API project.
+URL configuration for social_media_api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
