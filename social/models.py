fff13ae1376e814600415c690abb1e6bdd1a62b4
Rodion Prodaivoda <hapyto371@gmail.com>
2023-04-21 15:25:22 +0300

    added models

--- social/models.py
+++ social/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    username = models.CharField(max_length=63)
+    first_name = models.CharField(max_length=63)
+    last_name = models.CharField(max_length=63)
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="subscriptions"
+    )
+    target = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="subscribers"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('subscriber', 'target')
+
+    def __str__(self) -> str:
+        return f"{self.subscriber}"
+
+
+class Post(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
+    user_profile = models.ForeignKey(
+        Profile, on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return f"{self.message}"
